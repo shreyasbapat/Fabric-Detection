2ec71e4c75e5e6b1899b1ea9c6bd0d5da982a15e
shreyasbapat <bapat.shreyas@gmail.com>
2018-01-30 17:17:09 +0530

    Autoencoder works

--- rename.sh
+++ rename.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 read a
 counter=1
-cd Data/$a
+cd Fas/$a
 for f in $(ls -1)
 do
-  new_filename=$(printf "%d.jpg" ${counter})     
+  new_filename=$(printf "%d_.jpg" ${counter})     
   echo "renaming ${f} ..to.. ${new_filename}"
   mv ${f} ${new_filename}
   (( counter=${counter}+1 ))
